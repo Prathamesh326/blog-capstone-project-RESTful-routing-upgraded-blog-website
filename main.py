7ffb60636e5b78cccf8a78cfaf69530c5e6122a2
Prathamesh326 <110224953+Prathamesh326@users.noreply.github.com>
2024-08-04 02:01:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,6 @@ from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
@@ -65,21 +52,21 @@ with app.app_context():
 
 @app.route('/')
 def get_all_posts():
-    # TODO: Query the database for all the posts. Convert the data to a python list.
+    # Query the database for all the posts. Convert the data to a python list.
     res = db.session.execute(db.select(BlogPost))
     posts = res.scalars().all()
     return render_template("index.html", all_posts=posts)
 
 
-# TODO: Add a route so that you can click on individual posts.
+# Add a route so that you can click on individual posts.
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
-    # TODO: Retrieve a BlogPost from the database based on the post_id
+    # Retrieve a BlogPost from the database based on the post_id
     requested_post = db.get_or_404(BlogPost, post_id)
     return render_template("post.html", post=requested_post)
 
 
-# TODO: add_new_post() to create a new blog post
+# add_new_post() to create a new blog post
 @app.route('/new-post', methods=['GET', 'POST'])
 def add_new_post():
     form = NewPostForm()
@@ -98,7 +85,7 @@ def add_new_post():
     return render_template('make-post.html', form=form)
 
 
-# TODO: edit_post() to change an existing blog post
+# edit_post() to change an existing blog post
 @app.route("/edit-post/<int:post_id>", methods=['GET', 'POST'])
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
@@ -120,7 +107,7 @@ def edit_post(post_id):
     return render_template('make-post.html', form=edit_form, is_edit=True)
 
 
-# TODO: delete_post() to remove a blog post from the database
+# delete_post() to remove a blog post from the database
 @app.route("/delete/<post_id>", methods=["GET", "POST"])
 def delete_post(post_id):
     post_delete = db.get_or_404(BlogPost, post_id)
@@ -128,8 +115,6 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-
-# Below is the code from previous lessons. No changes needed.
 @app.route("/about")
 def about():
     return render_template("about.html")
